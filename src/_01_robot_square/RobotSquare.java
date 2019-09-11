9a91d62093317fb813bda1a61f990139185034c7
i-lab user <i-labuser@SDPL88533-iMacProLO.local>
2019-09-10 17:53:45 -0700

    robot square

--- src/_01_robot_square/RobotSquare.java
+++ src/_01_robot_square/RobotSquare.java
@@ -12,22 +12,25 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
-
+Robot sparkle =new Robot();
         // 3. Put the robot's pen down
-
+sparkle.penDown();
 
         // 6. Make the robot move as fast as possible
-
+sparkle.setSpeed(50);
 
         // 5. Do everything below here 4 times
-
-
+sparkle.move(200);
+sparkle.turn(-90);
+sparkle.move(200);
+sparkle.turn(-90);
+sparkle.move(200);
+sparkle.turn(-90);
         //         2. Move your robot 200 pixels
-
+sparkle.move(200);
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
-
+sparkle.turn(-90);
 
     }
 }
---
+++ src/_02_robot_graffiti/RobotGraffiti.java
@@ -0,0 +1,16 @@
+package _02_robot_graffiti;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotGraffiti {
+
+	public static void main(String[] args) {
+		Robot graffiti =new Robot();
+	graffiti.penDown();
+	graffiti.move(200);	
+	graffiti.turn(135);
+	graffiti.move(200);
+		// TODO Auto-generated method stub
+	}
+
+}

66bb221619f3902082e35c69079ed803c9c21e5e
i-lab user <i-labuser@SDPL88532-iMacProLO.local>
2019-10-15 17:55:18 -0700

    I learned alot about knock knock jokes

--- src/_03_morning_zombie/mnoringzombie.java
+++ src/_03_morning_zombie/mnoringzombie.java
@@ -1,7 +1,15 @@
 package _03_morning_zombie;
 
+import javax.swing.JOptionPane;
+
 public class mnoringzombie {
 public static void main(String[] args) {
-	
+	JOptionPane.showMessageDialog(null,"I wake up");
+	JOptionPane.showMessageDialog(null,"then I change");
+	JOptionPane.showMessageDialog(null, "after that I do my hair and brush it");
+	JOptionPane.showMessageDialog(null, "then I eat breakfest");
+	JOptionPane.showMessageDialog(null, "then I brush my teeth");
+	JOptionPane.showMessageDialog(null, "after that I put my shoes on");
+	JOptionPane.showMessageDialog(null, "then I go to school");
 }
 }
---
+++ src/_04_knock_knock/knockknock.java
@@ -0,0 +1,25 @@
+package _04_knock_knock;
+import javax.swing.JOptionPane;
+
+public class knockknock {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "knock");
+	JOptionPane.showMessageDialog(null,"knock");
+	JOptionPane.showMessageDialog(null,"who's");
+	JOptionPane.showMessageDialog(null, "there");
+	JOptionPane.showMessageDialog(null,"Mustache.");
+	JOptionPane.showMessageDialog(null,"Mustache");
+	JOptionPane.showMessageDialog(null,"who");
+	JOptionPane.showMessageDialog(null,"I");
+	JOptionPane.showMessageDialog(null,"Mustache");
+	JOptionPane.showMessageDialog(null,"you");
+	JOptionPane.showMessageDialog(null,"a");
+	JOptionPane.showMessageDialog(null,"question,");
+	JOptionPane.showMessageDialog(null,"but");
+	JOptionPane.showMessageDialog(null,"I'll");
+	JOptionPane.showMessageDialog(null,"shave");
+	JOptionPane.showMessageDialog(null,"it");
+	JOptionPane.showMessageDialog(null,"for");
+	JOptionPane.showMessageDialog(null,"later");
+}
+}

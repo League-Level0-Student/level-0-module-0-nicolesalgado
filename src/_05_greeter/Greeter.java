b166d5b89e7386507572870762e3499cd24b6d26
i-lab user <i-labuser@SDPL88534-iMacProL.local>
2019-10-29 17:56:59 -0700

    working on greeter

---
+++ src/_05_greeter/Greeter.java
@@ -0,0 +1,14 @@
+package _05_greeter;
+
+import javax.swing.JOptionPane;
+
+public class Greeter {
+
+	public static void main(String[] args) { 
+		// TODO Auto-generated method stub
+		String input =JOptionPane.showInputDialog("What is your name?");
+		JOptionPane.showMessageDialog(null,"Hello "+input);
+		
+	}
+
+}
---
+++ src/_06_everything_is_awesome/EverythingIsAwsome.java
@@ -0,0 +1,15 @@
+package _06_everything_is_awesome;
+
+import javax.swing.JOptionPane;
+
+public class EverythingIsAwsome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	String animal =	JOptionPane.showInputDialog("what stuff animal do you like?");
+		JOptionPane.showMessageDialog(null, animal + " is awsome");
+		
+	}
+	
+
+}

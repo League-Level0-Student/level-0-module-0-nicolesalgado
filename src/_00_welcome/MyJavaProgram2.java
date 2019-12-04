9cbb3df4c12425a01dc2d9b4194726a6fac41151
nicolesalgado <54606745+nicolesalgado@users.noreply.github.com>
2019-12-03 18:01:09 -0800

    hello

---
+++ src/_00_welcome/MyJavaProgram2.java
@@ -0,0 +1,14 @@
+package _00_welcome;
+import java.awt.Color;
+import org.jointheleague.graphical.robot.Robot;
+
+public class MyJavaProgram2 {
+	
+	public static void main(String[] args) {
+		
+		// START HERE
+	
+		
+		
+	}
+}
---
+++ src/_00_welcome/unbirthday.java
@@ -0,0 +1,9 @@
+package _00_welcome;
+
+import javax.swing.JOptionPane;
+
+public class unbirthday {
+public static void main(String[] args) {
+String bday = JOptionPane.showInputDialog("whens yourbirthday matt");
+}
+}

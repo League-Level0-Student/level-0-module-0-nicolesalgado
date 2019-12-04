7eaf330222de6fb05e85e41913c5388a91943644
nicolesalgado <54606745+nicolesalgado@users.noreply.github.com>
2019-12-03 17:21:19 -0800

    finish mojo 0

---
+++ src/_07_last_summer/lastsummer.java
@@ -0,0 +1,11 @@
+package _07_last_summer;
+
+import javax.swing.JOptionPane;
+
+public class lastsummer {
+public static void main(String[] args) {
+String answer =  JOptionPane.showInputDialog("whats your name?");
+String nicole =  JOptionPane.showInputDialog("what did you do last summer");
+JOptionPane.showMessageDialog(null, answer + " I know what you did last summer!!! "+nicole);
+}
+}
